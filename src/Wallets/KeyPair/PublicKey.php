0efb50fe57a3a4a2580ade5e41b6a9a442587e9c
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-03-31 19:51:22 +0500

    Public keys

--- src/Wallets/KeyPair/PublicKey.php
+++ src/Wallets/KeyPair/PublicKey.php
@@ -14,6 +14,8 @@ declare(strict_types=1);
 
 namespace FurqanSiddiqui\Bitcoin\Wallets\KeyPair;
 
+use FurqanSiddiqui\BIP32\Extend\PrivateKeyInterface;
+use FurqanSiddiqui\Bitcoin\Exception\AddressGenerateException;
 use FurqanSiddiqui\Bitcoin\Wallets\KeyPair\PublicKey\P2PKH_Address;
 
 /**
@@ -22,13 +24,36 @@ use FurqanSiddiqui\Bitcoin\Wallets\KeyPair\PublicKey\P2PKH_Address;
  */
 class PublicKey extends \FurqanSiddiqui\BIP32\KeyPair\PublicKey
 {
+    /** @var PrivateKey */
+    protected $privateKey;
+
     /**
-     * @param int $prefix
+     * PublicKey constructor.
+     * @param PrivateKeyInterface $keyPair
+     * @throws \FurqanSiddiqui\BIP32\Exception\PublicKeyException
+     * @throws \FurqanSiddiqui\ECDSA\Exception\GenerateVectorException
+     * @throws \FurqanSiddiqui\ECDSA\Exception\MathException
+     */
+    public function __construct(PrivateKeyInterface $keyPair)
+    {
+        if (!$keyPair instanceof PrivateKey) {
+            throw new \InvalidArgumentException('Public key instance can only be constructed via internal Private key');
+        }
+
+        parent::__construct($keyPair);
+    }
+
+    /**
      * @return P2PKH_Address
      * @throws \FurqanSiddiqui\Bitcoin\Exception\AddressGenerateException
      */
-    public function p2pkh(int $prefix): P2PKH_Address
+    public function p2pkh(): P2PKH_Address
     {
+        $prefix = $this->privateKey->node()->const_p2pkh_prefix;
+        if (!is_int($prefix)) {
+            throw new AddressGenerateException('P2PKH constant not defined');
+        }
+
         return new P2PKH_Address($this, $prefix);
     }
 }
