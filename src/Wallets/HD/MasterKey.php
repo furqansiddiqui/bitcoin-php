9e4d83a9039dd3c22826daee03e22436bcf41ee4
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-03-31 22:41:54 +0500

    HD master keys

--- src/Wallets/HD/MasterKey.php
+++ src/Wallets/HD/MasterKey.php
@@ -14,11 +14,28 @@ declare(strict_types=1);
 
 namespace FurqanSiddiqui\Bitcoin\Wallets\HD;
 
+use FurqanSiddiqui\Bitcoin\AbstractBitcoinNode;
+use FurqanSiddiqui\DataTypes\Binary;
+
 /**
  * Class MasterKey
  * @package FurqanSiddiqui\Bitcoin\Wallets\HD
  */
 class MasterKey extends ExtendedKeyPair
 {
+    /**
+     * MasterKey constructor.
+     * @param AbstractBitcoinNode $node
+     * @param Binary $seed
+     * @param string|null $hmacKey
+     * @throws \FurqanSiddiqui\BIP32\Exception\ExtendedKeyException
+     */
+    public function __construct(AbstractBitcoinNode $node, Binary $seed, ?string $hmacKey = null)
+    {
+        if ($hmacKey) {
+            $seed = $seed->hash()->hmac("sha512", $hmacKey);
+        }
 
+        parent::__construct($node, $seed, null);
+    }
 }
