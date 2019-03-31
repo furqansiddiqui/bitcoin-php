791c72dc36d9722eb592c2ec71c5e74800e74516
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-03-31 17:53:06 +0500

    Init class

---
+++ src/Bitcoin.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is a part of "furqansiddiqui/bitcoin-php" package.
+ * https://github.com/furqansiddiqui/bitcoin-php
+ *
+ * Copyright (c) 2019 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/furqansiddiqui/bitcoin-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\Bitcoin;
+
+use FurqanSiddiqui\Bitcoin\Networks;
+
+/**
+ * Class Bitcoin
+ * @package FurqanSiddiqui\Bitcoin
+ */
+class Bitcoin
+{
+    /**
+     * @return Networks\Bitcoin
+     */
+    public static function Mainnet(): Networks\Bitcoin
+    {
+        return new Networks\Bitcoin();
+    }
+
+    /**
+     * @return Networks\BitcoinTestnet
+     */
+    public static function Testnet(): Networks\BitcoinTestnet
+    {
+        return new Networks\BitcoinTestnet();
+    }
+}
