debc3263452dca02f52ae7b975e2532923a89bcb
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-05-29 17:16:59 +0500

    Transaction building

--- src/Transactions/SerializedTransaction.php
+++ src/Transactions/SerializedTransaction.php
@@ -22,26 +22,55 @@ use FurqanSiddiqui\DataTypes\Base16;
  */
 class SerializedTransaction
 {
+    /** @var Base16 */
     private $serializedBase16;
+    /** @var Base16 */
     private $hash;
 
+    /**
+     * SerializedTransaction constructor.
+     * @param Base16 $serializedTxBase16
+     * @param Base16|null $hash
+     */
     public function __construct(Base16 $serializedTxBase16, ?Base16 $hash = null)
     {
+        $this->serializedBase16 = $serializedTxBase16;
 
+        // Calculate hash
+        $this->hash = $this->serializedBase16->binary()
+            ->hash()->sha256()
+            ->hash()->sha256()// SHA256 twice
+            ->encode()->base16();
+
+        // Compare hash?
+        if ($hash) {
+            if (!$this->hash->equals($hash)) {
+                throw new \UnexpectedValueException('Serialized tx hash does not match with given arg hash');
+            }
+        }
     }
 
+    /**
+     * @return Base16
+     */
     public function get(): Base16
     {
         return $this->serializedBase16;
     }
 
+    /**
+     * @return Base16
+     */
     public function hash(): Base16
     {
         return $this->hash;
     }
 
+    /**
+     * @return Transaction
+     */
     public function decode(): Transaction
     {
-
+        return Transaction::Decode($this->serializedBase16);
     }
 }
--- src/Transactions/Transaction.php
+++ src/Transactions/Transaction.php
@@ -14,13 +14,17 @@ declare(strict_types=1);
 
 namespace FurqanSiddiqui\Bitcoin\Transactions;
 
+use FurqanSiddiqui\Bitcoin\Transactions\Transaction\TxInput;
 use FurqanSiddiqui\Bitcoin\Transactions\Transaction\TxInputs;
+use FurqanSiddiqui\Bitcoin\Transactions\Transaction\TxOutput;
 use FurqanSiddiqui\Bitcoin\Transactions\Transaction\TxOutputs;
 use FurqanSiddiqui\DataTypes\Base16;
 
 /**
  * Class Transaction
  * @package FurqanSiddiqui\Bitcoin\Transactions
+ * @property-read Base16 $verUInt32LE
+ * @property-read Base16 $lockTimeUInt32LE
  */
 class Transaction
 {
@@ -33,7 +37,7 @@ class Transaction
     /** @var int */
     private $lockTime;
 
-    public function Decode(Base16 $encodedTx): self
+    public static function Decode(Base16 $encodedTx): self
     {
 
     }
@@ -50,13 +54,43 @@ class Transaction
     }
 
     /**
+     * @param $prop
+     * @return Base16
+     */
+    public function __get($prop)
+    {
+        switch ($prop) {
+            case "verUInt32LE":
+                $uInt32LE = bin2hex(pack("V", $this->version));
+                return new Base16($uInt32LE);
+            case "lockTimeUInt32LE":
+                $uInt32LE = bin2hex(pack("V", $this->lockTime));
+                return new Base16($uInt32LE);
+        }
+
+        throw new \OutOfBoundsException('Cannot get value of inaccessible property');
+    }
+
+    /**
      * @param int $version
      * @return Transaction
      */
-    public function version(int $version): self
+    public function setVersion(int $version): self
     {
+        if ($version < 0) {
+            throw new \InvalidArgumentException('Invalid transaction version');
+        }
+
         $this->version = $version;
         return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function version(): int
+    {
+        return $this->version;
     }
 
     /**
@@ -79,14 +113,104 @@ class Transaction
      * @param int $lockTime
      * @return Transaction
      */
-    public function lockTime(int $lockTime): self
+    public function setLockTime(int $lockTime): self
     {
+        if ($lockTime < 0) {
+            throw new \InvalidArgumentException('Invalid transaction lock time');
+        }
+
         $this->lockTime = $lockTime;
         return $this;
     }
 
-    public function serialize()
+    /**
+     * @return int
+     */
+    public function lockTime(): int
     {
+        return $this->lockTime;
+    }
 
+    /**
+     * @return SerializedTransaction
+     */
+    public function serialize(): SerializedTransaction
+    {
+        $serialized = new Base16();
+
+        // Add 4 byte version
+        $serialized->append($this->verUInt32LE);
+
+        // Inputs
+        if ($this->inputs->count() < 1) {
+            throw new \UnexpectedValueException('Transaction has no inputs');
+        }
+
+        // Append number of inputs
+        $serialized->append(dechex($this->inputs->count()));
+
+        /** @var TxInput $input */
+        foreach ($this->inputs as $input) {
+            // Append 32 byte prev. Tx hash
+            $serialized->append($input->prevTxHash());
+
+            // Four byte output index
+            $serialized->append($input->indexUInt32LE);
+
+            // Signing scriptSig
+            $scriptSig = $input->scriptSig();
+            $scriptSigLen = $scriptSig->script()->binary()->size()->bytes();
+
+            // 1 byte scriptSig len
+            $serialized->append(dechex($scriptSigLen));
+
+            // Append actual scriptSig
+            $serialized->append($scriptSig->script());
+
+            // 4 byte sequence number
+            $serialized->append($input->seqUInt32LE);
+
+            // Input done! continue to next
+            unset($scriptSig, $scriptSigLen);
+        }
+
+        // Outputs
+        if ($this->outputs->count() < 1) {
+            throw new \UnexpectedValueException('Transaction has no outputs');
+        }
+
+        // Append number of outputs
+        $serialized->append(dechex($this->outputs->count()));
+
+        /** @var TxOutput $output */
+        foreach ($this->outputs as $output) {
+            // Output amount (8-byte)
+            $serialized->append($output->valueUInt64LE);
+
+            // scriptPubKey
+            $scriptPubKey = $output->scriptPubKey();
+            $scriptPubKeyLen = $scriptPubKey->script()->binary()->size()->bytes();
+
+            // 1 byte scriptPubKey len
+            $serialized->append(dechex($scriptPubKeyLen));
+
+            // Append actual scriptPubKey
+            $serialized->append($scriptPubKey->script());
+
+            // Output done!
+            unset($scriptPubKey, $scriptPubKeyLen);
+        }
+
+        // Finally, Tx lock time! 4 byte
+        $serialized->append($this->lockTimeUInt32LE);
+
+        // Set buffer state as readOnly
+        $serialized->readOnly(true);
+
+        // Calculate TxHash
+        $hash = $serialized->binary()->hash()->sha256()
+            ->hash()->sha256(); // SHA256 twice
+
+        return new SerializedTransaction($serialized, $hash->encode()->base16());
     }
 }
