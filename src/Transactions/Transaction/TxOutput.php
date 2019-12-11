47f7b85aea30bc5a9594c272f0621d73ccd517da
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-12-11 20:46:47 +0500

    address types based on script

--- src/Transactions/Transaction/TxOutput.php
+++ src/Transactions/Transaction/TxOutput.php
@@ -15,34 +15,62 @@ declare(strict_types=1);
 namespace FurqanSiddiqui\Bitcoin\Transactions\Transaction;
 
 use Comely\DataTypes\Buffer\Base16;
+use FurqanSiddiqui\Bitcoin\Exception\PaymentAddressException;
 use FurqanSiddiqui\Bitcoin\Script\Script;
+use FurqanSiddiqui\Bitcoin\Transactions\Transaction;
 
 /**
  * Class TxOutput
  * @package FurqanSiddiqui\Bitcoin\Transactions\Transaction
  * @property-read int $valueAsInt
  * @property-read Base16 $valueUInt64LE
+ * @property-read string|null $type
+ * @property-read string|null $address
+ * @property-read string|null $scriptError
  */
 class TxOutput implements TxInOutInterface
 {
+    /** @var Transaction */
+    private $tx;
+    /** @var int */
+    private $index;
     /** @var int */
     private $value;
     /** @var Script */
-    private $script;
+    private $scriptPubKey;
+    /** @var null|string */
+    private $type;
+    /** @var null|string */
+    private $address;
+    /** @var null|string */
+    private $scriptError;
 
     /**
      * TxOutput constructor.
+     * @param Transaction $tx
+     * @param int $index
      * @param int $satoshis
-     * @param Script $script
+     * @param Script $scriptPubKey
      */
-    public function __construct(int $satoshis, Script $script)
+    public function __construct(Transaction $tx, int $index, int $satoshis, Script $scriptPubKey)
     {
         if ($satoshis < 0) {
             throw new \InvalidArgumentException('Tx output value must be positive integer');
         }
 
+        $this->tx = $tx;
+        $this->index = $index;
         $this->value = $satoshis;
-        $this->script = $script;
+        $this->scriptPubKey = $scriptPubKey;
+
+        // Convert ScriptPubKey to address and appropriate address-type
+        try {
+            $address = $tx->network->address()->addressFromScript($scriptPubKey);
+            $this->type = $address->type();
+            $this->address = $address->address();
+        } catch (PaymentAddressException $e) {
+            $this->scriptError = $e->getMessage();
+        }
     }
 
     /**
@@ -58,9 +86,21 @@ class TxOutput implements TxInOutInterface
                 // Convert value to uint64 little endian
                 $uInt64LE = bin2hex(pack("P", $this->value));
                 return new Base16($uInt64LE);
+            case "type":
+            case "address":
+            case "scriptError":
+                return $this->$prop;
         }
 
         throw new \OutOfBoundsException('Cannot get value of inaccessible property');
+    }
+
+    /**
+     * @return array
+     */
+    public function __debugInfo(): array
+    {
+        return $this->dump();
     }
 
     /**
@@ -74,17 +114,9 @@ class TxOutput implements TxInOutInterface
     /**
      * @return Script
      */
-    public function script(): Script
-    {
-        return $this->script;
-    }
-
-    /**
-     * @return Script
-     */
     public function scriptPubKey(): Script
     {
-        return $this->script;
+        return $this->scriptPubKey;
     }
 
     /**
@@ -92,16 +124,23 @@ class TxOutput implements TxInOutInterface
      */
     public function dump(): array
     {
-        return [
+        $dump = [
             "value" => [
                 "dec" => $this->valueAsInt,
                 "uInt64LE" => $this->valueUInt64LE->hexits(false)
             ],
             "script" => [
-                "script" => $this->script->raw(),
-                "base16" => $this->script->script()->hexits(false),
-                "type" => null,
+                "script" => $this->scriptPubKey->raw(),
+                "base16" => $this->scriptPubKey->script()->hexits(false),
+                "type" => $this->type,
+                "address" => $this->address,
             ]
         ];
+
+        if ($this->scriptError) {
+            $dump["script"]["error"] = $this->scriptError;
+        }
+
+        return $dump;
     }
 }
