1a4d06492c33cc8f65a9f67cd49ecd411bb64ddd
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2023-03-09 20:33:47 +0400

    keypair

--- src/Wallets/KeyPair/PrivateKey.php
+++ src/Wallets/KeyPair/PrivateKey.php
@@ -1,9 +1,9 @@
 <?php
-/**
+/*
  * This file is a part of "furqansiddiqui/bitcoin-php" package.
  * https://github.com/furqansiddiqui/bitcoin-php
  *
- * Copyright (c) 2019-2020 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *  Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code or visit following link:
@@ -14,12 +14,9 @@ declare(strict_types=1);
 
 namespace FurqanSiddiqui\Bitcoin\Wallets\KeyPair;
 
-use Comely\DataTypes\Buffer\Base16;
-use FurqanSiddiqui\BIP32\Extend\ExtendedKeyInterface;
-use FurqanSiddiqui\BIP32\Extend\PublicKeyInterface;
-use FurqanSiddiqui\Bitcoin\AbstractBitcoinNode;
-use FurqanSiddiqui\Bitcoin\Wallets\KeyPair\PrivateKey\Export;
-use FurqanSiddiqui\Bitcoin\Wallets\KeyPair\PrivateKey\Signer;
+use Comely\Buffer\Buffer;
+use FurqanSiddiqui\Bitcoin\Bitcoin;
+use FurqanSiddiqui\ECDSA\KeyPair;
 
 /**
  * Class PrivateKey
@@ -27,60 +24,27 @@ use FurqanSiddiqui\Bitcoin\Wallets\KeyPair\PrivateKey\Signer;
  */
 class PrivateKey extends \FurqanSiddiqui\BIP32\KeyPair\PrivateKey
 {
-    /** @var AbstractBitcoinNode */
-    private $node;
-
     /**
-     * PrivateKey constructor.
-     * @param AbstractBitcoinNode $node
-     * @param Base16 $entropy
-     * @param ExtendedKeyInterface|null $extendedKey
+     * @param \FurqanSiddiqui\Bitcoin\Bitcoin $btc
+     * @param \FurqanSiddiqui\ECDSA\KeyPair $eccPrivateKey
      */
-    public function __construct(AbstractBitcoinNode $node, Base16 $entropy, ?ExtendedKeyInterface $extendedKey = null)
+    public function __construct(
+        public readonly Bitcoin $btc,
+        KeyPair                 $eccPrivateKey,
+    )
     {
-        $this->node = $node;
-        parent::__construct($entropy, $extendedKey);
-
-        // Configure ECDSA Curve
-        if (!$extendedKey && $this->node->const_ecdsa_curve) {
-            $this->set("curve", $this->node->const_ecdsa_curve);
-        }
+        parent::__construct($this->btc->bip32, $eccPrivateKey);
     }
 
     /**
-     * @return AbstractBitcoinNode
+     * @return string
      */
-    public function node(): AbstractBitcoinNode
+    public function exportWIF(): string
     {
-        return $this->node;
-    }
+        $raw = (new Buffer($this->eccPrivateKey->private->raw()))
+            ->append("\01") // Compressed Public Key Flag
+            ->prependUInt8($this->btc->network->wif_prefix); // Network prefix
 
-    /**
-     * @return PublicKey
-     * @throws \FurqanSiddiqui\BIP32\Exception\PublicKeyException
-     */
-    public function publicKey(): PublicKeyInterface
-    {
-        if (!$this->publicKey instanceof PublicKey) {
-            $this->publicKey = new PublicKey($this->node, $this);
-        }
-
-        return $this->publicKey;
-    }
-
-    /**
-     * @return Signer
-     */
-    public function sign(): Signer
-    {
-        return new Signer($this);
-    }
-
-    /**
-     * @return Export
-     */
-    public function export(): Export
-    {
-        return new Export($this);
+        return $this->btc->base58()->checkEncode($raw);
     }
 }
--- src/Wallets/KeyPair/PublicKey.php
+++ src/Wallets/KeyPair/PublicKey.php
@@ -1,9 +1,9 @@
 <?php
-/**
+/*
  * This file is a part of "furqansiddiqui/bitcoin-php" package.
  * https://github.com/furqansiddiqui/bitcoin-php
  *
- * Copyright (c) 2019-2020 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *  Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code or visit following link:
@@ -14,17 +14,12 @@ declare(strict_types=1);
 
 namespace FurqanSiddiqui\Bitcoin\Wallets\KeyPair;
 
-use Comely\DataTypes\Buffer\Base16;
-use FurqanSiddiqui\Base58\Result\Base58Encoded;
-use FurqanSiddiqui\BIP32\Extend\PrivateKeyInterface;
-use FurqanSiddiqui\Bitcoin\AbstractBitcoinNode;
+use Comely\Buffer\Buffer;
+use Comely\Buffer\Bytes20;
+use FurqanSiddiqui\Bitcoin\Bitcoin;
 use FurqanSiddiqui\Bitcoin\Address\P2PKH_Address;
 use FurqanSiddiqui\Bitcoin\Address\P2SH_Address;
 use FurqanSiddiqui\Bitcoin\Address\P2SH_P2WPKH_Address;
-use FurqanSiddiqui\Bitcoin\Exception\KeyPairExportException;
-use FurqanSiddiqui\Bitcoin\Serialize\Base58Check;
-use FurqanSiddiqui\Bitcoin\Wallets\KeyPair\PublicKey\Verifier;
-use FurqanSiddiqui\ECDSA\ECC\EllipticCurveInterface;
 
 /**
  * Class PublicKey
@@ -32,145 +27,61 @@ use FurqanSiddiqui\ECDSA\ECC\EllipticCurveInterface;
  */
 class PublicKey extends \FurqanSiddiqui\BIP32\KeyPair\PublicKey
 {
-    /** @var AbstractBitcoinNode */
-    private $node;
-    /** @var null|PrivateKey */
-    protected $privateKey;
-    /** @var null|Base16 */
-    private $hash160;
+    public readonly Bytes20 $hash160;
 
     /**
-     * PublicKey constructor.
-     * @param AbstractBitcoinNode $node
-     * @param PrivateKeyInterface|null $privateKey
-     * @param EllipticCurveInterface|null $curve
-     * @param Base16|null $publicKey
-     * @param bool|null $pubKeyArgIsCompressed
-     * @throws \FurqanSiddiqui\BIP32\Exception\PublicKeyException
+     * @param \FurqanSiddiqui\Bitcoin\Bitcoin $btc
+     * @param \FurqanSiddiqui\ECDSA\ECC\PublicKey $eccPublicKey
      */
-    public function __construct(AbstractBitcoinNode $node, ?PrivateKeyInterface $privateKey, ?EllipticCurveInterface $curve = null, ?Base16 $publicKey = null, ?bool $pubKeyArgIsCompressed = null)
+    public function __construct(
+        public readonly Bitcoin             $btc,
+        \FurqanSiddiqui\ECDSA\ECC\PublicKey $eccPublicKey
+    )
     {
-        $this->node = $node;
-        parent::__construct($privateKey, $curve, $publicKey, $pubKeyArgIsCompressed);
+        parent::__construct($this->btc->bip32, $eccPublicKey);
+        $this->hash160 = $this->btc->network->hash160($this->compressed());
     }
 
     /**
-     * @param AbstractBitcoinNode $network
-     * @return PublicKey
-     */
-    public function setBitcoinNetworkInstance(AbstractBitcoinNode $network): self
-    {
-        $this->node = $network;
-        return $this;
-    }
-
-    /**
-     * @return AbstractBitcoinNode
-     */
-    public function node(): AbstractBitcoinNode
-    {
-        return $this->node;
-    }
-
-    /**
-     * @return PrivateKey
-     */
-    public function privateKey(): PrivateKeyInterface
-    {
-        return parent::privateKey();
-    }
-
-    /**
-     * @return Base16
-     */
-    public function hash160(): Base16
-    {
-        if (!$this->hash160) {
-            $hash160 = $this->compressed()->clone();
-            $hash160 = $hash160->binary()->hash()->sha256()
-                ->hash()->ripeMd160();
-
-            $this->hash160 = $hash160->base16();
-            $this->hash160->readOnly(true);
-        }
-
-        return $this->hash160;
-    }
-
-    /**
-     * @return Verifier
-     */
-    public function verify(): Verifier
-    {
-        return new Verifier($this);
-    }
-
-    /**
-     * @param int|null $prefix
-     * @return Base58Encoded
-     * @throws KeyPairExportException
-     */
-    public function exportBIP32(?int $prefix = null): Base58Encoded
-    {
-        $prefix = $prefix ?? $this->privateKey->node()->const_bip32_public_prefix;
-        if (!is_int($prefix)) {
-            throw new KeyPairExportException('BIP32 public key prefix constant not defined');
-        }
-
-        $ekd = $this->privateKey->ekd();
-        if (!$ekd) {
-            throw new KeyPairExportException('This public key is not HD/BIP32 based');
-        }
-
-        return Base58Check::getInstance()->encode($ekd->serializePublicKey($prefix)->base16());
-    }
-
-    /**
-     * @param int|null $prefix
-     * @return P2PKH_Address
+     * @return \FurqanSiddiqui\Bitcoin\Address\P2PKH_Address
      * @throws \FurqanSiddiqui\Bitcoin\Exception\PaymentAddressException
      */
-    public function p2pkh(?int $prefix = null): P2PKH_Address
+    public function p2pkh(): P2PKH_Address
     {
-        $base58Check = Base58Check::getInstance();
-        $prefix = $prefix ?? $this->node->const_p2pkh_prefix;
-
-        $rawP2PKH = $this->hash160()->clone();
-        if (is_int($prefix) && $prefix >= 0) {
-            $rawP2PKH->prepend(dechex($prefix));
-        }
-
-        return new P2PKH_Address($this->node, $base58Check->encode($rawP2PKH)->value(), $this->hash160());
+        $rawP2PKH = (new Buffer($this->hash160->raw()))->prependUInt8($this->btc->network->p2pkh_prefix);
+        return new P2PKH_Address($this->btc, $this->btc->bip32->base58->checkEncode($rawP2PKH));
     }
 
     /**
-     * @return P2SH_Address
+     * @return \FurqanSiddiqui\Bitcoin\Address\P2SH_Address
      * @throws \FurqanSiddiqui\Bitcoin\Exception\PaymentAddressException
+     * @throws \FurqanSiddiqui\Bitcoin\Exception\ScriptDecodeException
      * @throws \FurqanSiddiqui\Bitcoin\Exception\ScriptParseException
      */
     public function p2sh(): P2SH_Address
     {
-        $redeemScript = $this->node->script()->new()
-            ->PUSHDATA($this->compressed()->binary())
+        $redeemScript = $this->btc->scripts->new()
+            ->PUSHDATA($this->compressed())
             ->OP_CHECKSIG()
-            ->script();
+            ->getScript();
 
-        return $this->node->p2sh()->fromRedeemScript($redeemScript);
+        return $this->btc->address->fromRedeemScript($redeemScript);
     }
 
     /**
-     * @return P2SH_P2WPKH_Address
+     * @return \FurqanSiddiqui\Bitcoin\Address\P2SH_P2WPKH_Address
      * @throws \FurqanSiddiqui\Bitcoin\Exception\PaymentAddressException
+     * @throws \FurqanSiddiqui\Bitcoin\Exception\ScriptDecodeException
      * @throws \FurqanSiddiqui\Bitcoin\Exception\ScriptParseException
      */
     public function p2sh_P2WPKH(): P2SH_P2WPKH_Address
     {
-        $redeemScript = $this->node->script()->new()
+        $redeemScript = $this->btc->scripts->new()
             ->OP_0()
-            ->PUSHDATA($this->hash160()->binary())
-            ->script();
+            ->PUSHDATA($this->hash160)
+            ->getScript();
 
-        $p2sh = $this->node->p2sh()->fromRedeemScript($redeemScript);
-        return new P2SH_P2WPKH_Address($this->node, $p2sh->address(), $p2sh->hash160(), $redeemScript);
+        $p2sh = $this->btc->address->fromRedeemScript($redeemScript);
+        return new P2SH_P2WPKH_Address($this->btc, $p2sh->address);
     }
 }
