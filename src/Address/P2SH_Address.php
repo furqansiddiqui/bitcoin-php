f501379a4b45ed0a030ea98dea99fbe9efce28ef
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2023-03-09 19:30:49 +0400

    P2SH addresses

--- src/Address/P2SH_Address.php
+++ src/Address/P2SH_Address.php
@@ -1,9 +1,9 @@
 <?php
-/**
+/*
  * This file is a part of "furqansiddiqui/bitcoin-php" package.
  * https://github.com/furqansiddiqui/bitcoin-php
  *
- * Copyright (c) 2019-2020 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *  Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code or visit following link:
@@ -14,8 +14,6 @@ declare(strict_types=1);
 
 namespace FurqanSiddiqui\Bitcoin\Address;
 
-use Comely\DataTypes\Buffer\Base16;
-use FurqanSiddiqui\Bitcoin\AbstractBitcoinNode;
 use FurqanSiddiqui\Bitcoin\Exception\PaymentAddressException;
 use FurqanSiddiqui\Bitcoin\Script\Script;
 
@@ -23,70 +21,31 @@ use FurqanSiddiqui\Bitcoin\Script\Script;
  * Class P2SH_Address
  * @package FurqanSiddiqui\Bitcoin\Address
  */
-class P2SH_Address extends AbstractPaymentAddress
+class P2SH_Address extends AbstractBase58Address
 {
-    protected const TYPE = "p2sh";
-
-    /** @var Script|null */
-    private $redeemScript;
-
     /**
-     * P2SH_Address constructor.
-     * @param AbstractBitcoinNode|null $node
-     * @param string $addr
-     * @param Base16|null $crossCheckHash160
-     * @param Script|null $redeemScript
-     * @throws PaymentAddressException
+     * @return void
+     * @throws \FurqanSiddiqui\Bitcoin\Exception\PaymentAddressException
      */
-    public function __construct(?AbstractBitcoinNode $node, string $addr, ?Base16 $crossCheckHash160 = null, ?Script $redeemScript = null)
+    protected function validateAddrPrefix(): void
     {
-        parent::__construct($node, $addr, $crossCheckHash160);
-
-        // Prefix verify
-        if ($node) {
-            $nodeP2SH_Prefix = dechex($node->const_p2sh_prefix);
-            if (strlen($nodeP2SH_Prefix) % 2 !== 0) {
-                $nodeP2SH_Prefix = "0" . $nodeP2SH_Prefix;
-            }
-
-            if ($this->prefix->hexits(false) !== $nodeP2SH_Prefix) {
-                throw new PaymentAddressException('Payment address P2SH prefix does not match');
-            }
+        if ($this->prefix !== $this->btc->network->p2sh_prefix) {
+            throw new PaymentAddressException(sprintf('P2SH address does not belong to %s', $this->btc->network->name));
         }
-
-        // Redeem Script
-        $this->redeemScript = $redeemScript;
     }
 
     /**
-     * @param Script $redeemScript
-     * @return $this
-     */
-    public function setRedeemScript(Script $redeemScript): self
-    {
-        $this->redeemScript = $redeemScript;
-        return $this;
-    }
-
-    /**
-     * @return Script|null
-     */
-    public function getRedeemScript(): ?Script
-    {
-        return $this->redeemScript;
-    }
-
-    /**
-     * @return Script|null
+     * @return \FurqanSiddiqui\Bitcoin\Script\Script
+     * @throws \FurqanSiddiqui\Bitcoin\Exception\ScriptDecodeException
      * @throws \FurqanSiddiqui\Bitcoin\Exception\ScriptParseException
      */
-    public function scriptPubKey(): ?Script
+    public function scriptPubKey(): Script
     {
-        $opCode = $this->node->opCode()->new();
-        $opCode->OP_HASH160()
-            ->PUSHDATA($this->hash160()->binary())
+        $opCode = $this->btc->scripts->new()
+            ->OP_HASH160()
+            ->PUSHDATA($this->raw)
             ->OP_EQUAL();
 
-        return $opCode->script();
+        return $opCode->getScript();
     }
 }
--- src/Address/P2SH_P2WSH_Address.php
+++ src/Address/P2SH_P2WSH_Address.php
@@ -1,9 +1,9 @@
 <?php
-/**
+/*
  * This file is a part of "furqansiddiqui/bitcoin-php" package.
  * https://github.com/furqansiddiqui/bitcoin-php
  *
- * Copyright (c) 2019-2020 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *  Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code or visit following link:
@@ -14,28 +14,10 @@ declare(strict_types=1);
 
 namespace FurqanSiddiqui\Bitcoin\Address;
 
-use Comely\DataTypes\Buffer\Base16;
-use FurqanSiddiqui\Bitcoin\AbstractBitcoinNode;
-use FurqanSiddiqui\Bitcoin\Script\Script;
-
 /**
  * Class P2SH_P2WSH_Address
  * @package FurqanSiddiqui\Bitcoin\Address
  */
 class P2SH_P2WSH_Address extends P2SH_Address
 {
-    public const TYPE = "p2sh-p2wsh";
-
-    /**
-     * P2SH_P2WSH_Address constructor.
-     * @param AbstractBitcoinNode|null $node
-     * @param string $addr
-     * @param Base16|null $crossCheckHash160
-     * @param Script|null $redeemScript
-     * @throws \FurqanSiddiqui\Bitcoin\Exception\PaymentAddressException
-     */
-    public function __construct(?AbstractBitcoinNode $node, string $addr, ?Base16 $crossCheckHash160 = null, Script $redeemScript = null)
-    {
-        parent::__construct($node, $addr, $crossCheckHash160, $redeemScript);
-    }
 }
