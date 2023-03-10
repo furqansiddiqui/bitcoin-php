903575142e5142b5eb78ec166e55cca1d204d60a
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2023-03-10 21:57:06 +0400

    txn decoder

--- src/Transactions/RawTransactionDecoder.php
+++ src/Transactions/RawTransactionDecoder.php
@@ -1,9 +1,9 @@
 <?php
-/**
+/*
  * This file is a part of "furqansiddiqui/bitcoin-php" package.
  * https://github.com/furqansiddiqui/bitcoin-php
  *
- * Copyright (c) 2019-2020 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *  Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code or visit following link:
@@ -14,14 +14,16 @@ declare(strict_types=1);
 
 namespace FurqanSiddiqui\Bitcoin\Transactions;
 
-use Comely\DataTypes\Buffer\Base16;
-use Comely\DataTypes\Buffer\Binary\ByteReader;
-use FurqanSiddiqui\Bitcoin\AbstractBitcoinNode;
+use Comely\Buffer\Buffer;
+use Comely\Buffer\ByteReader;
+use Comely\Buffer\Bytes32;
+use Comely\Buffer\Exception\ByteReaderUnderflowException;
+use FurqanSiddiqui\Bitcoin\Bitcoin;
 use FurqanSiddiqui\Bitcoin\Exception\ScriptException;
 use FurqanSiddiqui\Bitcoin\Exception\TransactionDecodeException;
 use FurqanSiddiqui\Bitcoin\Protocol\VarInt;
 use FurqanSiddiqui\Bitcoin\Script\Script;
-use FurqanSiddiqui\Bitcoin\Transactions\Transaction\TxInput;
+use FurqanSiddiqui\Bitcoin\Transactions\UTXO\TxInput;
 
 /**
  * Class RawTransactionDecoder
@@ -29,67 +31,61 @@ use FurqanSiddiqui\Bitcoin\Transactions\Transaction\TxInput;
  */
 class RawTransactionDecoder
 {
+    /** @var string[] */
     private const INPUT_SCRIPT_PUB_KEYS = [
         "/^76a914[a-f0-9]{40}88ac$/i",
         "/^a914[a-f0-9]{40}87$/i",
     ];
 
     /**
-     * @param AbstractBitcoinNode $network
-     * @param Base16 $rawTx
-     * @return Transaction
-     * @throws TransactionDecodeException
+     * @param \FurqanSiddiqui\Bitcoin\Bitcoin $btc
+     * @param \Comely\Buffer\Buffer $rawTx
+     * @return \FurqanSiddiqui\Bitcoin\Transactions\Transaction
+     * @throws \FurqanSiddiqui\Bitcoin\Exception\TransactionDecodeException
      */
-    public static function Decode(AbstractBitcoinNode $network, Base16 $rawTx): Transaction
+    public static function Decode(Bitcoin $btc, Buffer $rawTx): Transaction
     {
-        // Prepare
-        $tx = new Transaction($network);
-        $rawTx = $rawTx->binary();
+        $tx = new Transaction($btc);
         $rawTxStream = $rawTx->read();
-        $rawTxStream->throwUnderflowEx(); // Throws \UnderflowException if runs out of bytes
-        $txIsSegWit = false;
 
         try {
             $decodeProgress = "version bytes";
             // Start with version, first 4 bytes
-            $versionInt = VarInt::Decode(bin2hex($rawTxStream->first(4)), 4);
-            if (!isset($versionInt) || !is_int($versionInt)) {
+            $tx->version = $rawTxStream->readUInt32LE();
+            if ($tx->version < 0) {
                 throw TransactionDecodeException::InvalidVersion();
             }
 
-            $tx->setVersion($versionInt);
-
             // SegWit flag?
             $decodeProgress = "SegWit flag";
-            $segWitFlag = $rawTxStream->next(2);
-            if (bin2hex($segWitFlag) === "0001") {
-                $txIsSegWit = true;
-                $tx->isSegWit(); // Mark SegWit flag
-            } else {
-                $rawTxStream->setPointer(4); // Reverse pointer
+            if ($rawTxStream->lookAhead(2) === "\0\1") {
+                $rawTxStream->next(2); // Increase pointer
+                $tx->isSegWit = true;
             }
 
             // Inputs
             $decodeProgress = "inputs";
-            $inputsCount = self::readNextVarInt($rawTxStream);
+            $inputsCount = static::readNextVarInt($rawTxStream);
             if ($inputsCount) {
                 for ($i = 1; $i <= $inputsCount; $i++) {
+                    unset($prevTxHash, $prevTxIndex, $scriptLen, $txInput, $inputScript, $inputScriptPubKey, $inputScriptSig, $inputScriptHex, $seqNo);
+
                     $decodeProgress = sprintf("input # %d", $i);
-                    $prevTxHash = implode("", array_reverse(str_split(bin2hex($rawTxStream->next(32)), 2)));
-                    $prevTxIndex = VarInt::Decode(bin2hex($rawTxStream->next(4)), 4);
+                    $prevTxHash = new Bytes32(implode("", array_reverse(str_split($rawTxStream->next(32), 1))));
+                    $prevTxIndex = $rawTxStream->readUInt32LE();
                     $inputScript = null;
                     $inputScriptSig = null;
                     $inputScriptPubKey = null;
-                    $scriptLen = self::readNextVarInt($rawTxStream);
+                    $scriptLen = static::readNextVarInt($rawTxStream);
                     if ($scriptLen) {
                         try {
-                            $inputScript = Script::Decode($network, bin2hex($rawTxStream->next($scriptLen)));
+                            $inputScript = Script::Decode($btc, bin2hex($rawTxStream->next($scriptLen)));
                         } catch (ScriptException $e) {
                             throw TransactionDecodeException::InputScriptParseException($i, $e);
                         }
 
                         // Determine script Type
-                        $inputScriptHex = $inputScript->script()->hexits(false);
+                        $inputScriptHex = $inputScript->buffer->toBase16();
                         foreach (self::INPUT_SCRIPT_PUB_KEYS as $inputScriptExp) {
                             if (preg_match($inputScriptExp, $inputScriptHex)) {
                                 $inputScriptPubKey = $inputScript;
@@ -100,42 +96,38 @@ class RawTransactionDecoder
                         if (!$inputScriptPubKey) {
                             $inputScriptSig = $inputScript;
                         }
-
                     }
 
-                    $seqNo = VarInt::Decode(bin2hex($rawTxStream->next(4)), 4);
+                    $seqNo = $rawTxStream->readUInt32LE();
                     $txInput = $tx->inputs()->add($prevTxHash, $prevTxIndex, $inputScriptPubKey, $seqNo);
                     if ($inputScriptSig) {
                         $txInput->setScriptSig($inputScript);
                     }
-
-                    unset($prevTxHash, $prevTxIndex, $scriptLen, $txInput, $inputScript, $inputScriptPubKey, $inputScriptSig, $inputScriptHex, $seqNo);
                 }
             }
 
             // Outputs
             $decodeProgress = "outputs";
-            $outputsCount = self::readNextVarInt($rawTxStream);
+            $outputsCount = static::readNextVarInt($rawTxStream);
             for ($i = 0; $i < $outputsCount; $i++) {
                 $decodeProgress = sprintf("output # %d", $i);
                 try {
-                    $value = VarInt::Decode(bin2hex($rawTxStream->next(8)), 8);
-                    $scriptLen = self::readNextVarInt($rawTxStream);
-                    $script = bin2hex($rawTxStream->next($scriptLen));
-                    $tx->outputs()->add(Script::Decode($network, $script), $value);
+                    $value = $rawTxStream->readUInt64LE();
+                    $scriptLen = static::readNextVarInt($rawTxStream);
+                    $tx->outputs()->add(Script::Decode($btc, new Buffer($rawTxStream->next($scriptLen))), $value);
                 } catch (ScriptException $e) {
                     throw TransactionDecodeException::OutputScriptParseException($i, $e);
                 }
             }
 
             // Segregated Witness Data?
-            if ($txIsSegWit) {
+            if ($tx->isSegWit) {
                 $inputNo = 0;
                 /** @var TxInput $input */
-                foreach ($tx->inputs()->all() as $input) {
+                foreach ($tx->getInputs() as $input) {
                     $inputNo++;
                     $decodeProgress = sprintf("witness # %d", $inputNo);
-                    $witElemCount = self::readNextVarInt($rawTxStream);
+                    $witElemCount = static::readNextVarInt($rawTxStream);
                     if (!$witElemCount) {
                         throw TransactionDecodeException::WitnessParseException(
                             sprintf('Expecting SegWit witness data for input # %d', $inputNo)
@@ -143,9 +135,9 @@ class RawTransactionDecoder
                     }
 
                     for ($i = 1; $i <= $witElemCount; $i++) {
-                        $witElemLength = self::readNextVarInt($rawTxStream);
+                        $witElemLength = static::readNextVarInt($rawTxStream);
                         if ($witElemLength) {
-                            $input->setWitnessData(new Base16(bin2hex($rawTxStream->next($witElemLength))));
+                            $input->setWitnessData(new Buffer($rawTxStream->next($witElemLength)));
                         }
                     }
 
@@ -156,12 +148,10 @@ class RawTransactionDecoder
             // Lock time
             $decodeProgress = "lock time";
             $tx->setLockTime(VarInt::Decode(bin2hex($rawTxStream->next(4)), 4));
-        } catch (\UnderflowException $e) {
-            if ($e->getCode() === ByteReader::UNDERFLOW_EX_SIGNAL) {
-                throw new TransactionDecodeException(
-                    sprintf('Incomplete transaction data; Ran out of bytes near "%s"', $decodeProgress)
-                );
-            }
+        } catch (ByteReaderUnderflowException) {
+            throw new TransactionDecodeException(
+                sprintf('Incomplete transaction data; Ran out of bytes near "%s"', $decodeProgress)
+            );
         }
 
         // Excess bytes?
@@ -173,17 +163,18 @@ class RawTransactionDecoder
     }
 
     /**
-     * @param ByteReader $stream
+     * @param \Comely\Buffer\ByteReader $stream
      * @return int
+     * @throws \Comely\Buffer\Exception\ByteReaderUnderflowException
      */
     private static function readNextVarInt(ByteReader $stream): int
     {
-        $varInt = bin2hex($stream->next(1)); // Next 1 byte
-        if (in_array(strtolower($varInt), ["fd", "fe", "ff"])) {
-            $varIntSize = VarInt::Size($varInt);
-            return VarInt::Decode(bin2hex($stream->next($varIntSize)), $varIntSize);
-        } else {
-            return hexdec($varInt);
-        }
+        $varInt = $stream->readUInt8();
+        return match ($varInt) {
+            253 => $stream->readUInt16LE(),
+            254 => $stream->readUInt32LE(),
+            255 => $stream->readUInt64LE(),
+            default => $varInt
+        };
     }
 }
